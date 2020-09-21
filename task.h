8dc3ead8399425811d0f7eaed80a910a2e755d49
david  musaev <vadim566@gmail.com>
2020-09-21 20:10:59 +0300

    task class
    added task.h and task.cpp

--- task.h
+++ task.h
@@ -2,22 +2,45 @@
 #include <iostream>
 #include <string>
 
+using namespace std;
 class task
 {
 public:
+	//constructors
+	task(string initDate,string finalDate,string name);
 	task();
-	~task();
+	
+	//get set name
+	void setName(string name);
+	string getName()const { return name; };
+
+	//get set initDate of a task
+	void setInitDate(string initDate);
+	string getInitDate()const { return initDate; };
+
+	//get set finalDate of a task
+	void setFinalDate(string finalDate);
+	string getFinalDate()const { return finalDate; };
+
+	//get unique number of task
+	int getNumberOfTask()const { return numberTask; };
+	
+	//get total count of task in the system
+	static int getTaskCt(){ return taskCt; };
+
+
+	//pure virtual print
+	virtual void print(int taskNumber) const = 0;
+
+	
 
 private:
+	
 	string initDate;
-
-
+	string finalDate;
+	string name;
+	const int numberTask;
+	static int taskCt;
 };
 
-task::task()
-{
-}
 
-task::~task()
-{
-}
